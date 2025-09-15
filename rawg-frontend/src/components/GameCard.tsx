0d53aff636fb27b959e88840fab1457e72e37a01
pesekt1 <pesekt@gmail.com>
2025-09-15 10:16:03 +0200

    created metacritic badge in the game card

--- rawg-frontend/src/components/GameCard.tsx
+++ rawg-frontend/src/components/GameCard.tsx
@@ -1,6 +1,7 @@
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
 
 import type { Game } from "../hooks/useGames";
+import CriticScore from "./CriticScore";
 import PlatformIconsList from "./PlatformIconsList";
 
 interface Props {
@@ -11,9 +12,12 @@ export const GameCard = ({ game }: Props) => {
   return (
     <Card borderRadius={10} overflow="hidden">
       <Image src={game.background_image} alt={game.name} />
-      <PlatformIconsList
-        platforms={game.parent_platforms.map((p) => p.platform)}
-      />
+      <HStack justifyContent="space-between" padding={2}>
+        <PlatformIconsList
+          platforms={game.parent_platforms.map((p) => p.platform)}
+        />
+        <CriticScore score={game.metacritic} />
+      </HStack>
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
       </CardBody>
