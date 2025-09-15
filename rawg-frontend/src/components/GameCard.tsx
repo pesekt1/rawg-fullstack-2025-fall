18afd0bee07a851821f221b2849aba0616a4642e
pesekt1 <pesekt@gmail.com>
2025-09-15 09:07:56 +0200

    showing a list of parent platforms in game card

--- rawg-frontend/src/components/GameCard.tsx
+++ rawg-frontend/src/components/GameCard.tsx
@@ -1,4 +1,4 @@
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
 import type { Game } from "../hooks/useGames";
 
 interface Props {
@@ -9,6 +9,9 @@ export const GameCard = ({ game }: Props) => {
   return (
     <Card borderRadius={10} overflow="hidden">
       <Image src={game.background_image} alt={game.name} />
+      {game.parent_platforms.map((platform) => (
+        <Text key={platform.platform.id}>{platform.platform.name}</Text>
+      ))}
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
       </CardBody>
