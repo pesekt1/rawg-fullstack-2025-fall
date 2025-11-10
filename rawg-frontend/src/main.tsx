e8ecf97a490e779e9d92d32e9431e441a1c26bd7
pesekt1 <pesekt@gmail.com>
2025-11-10 08:57:23 +0100

    ms library for time in ms

--- rawg-frontend/src/main.tsx
+++ rawg-frontend/src/main.tsx
@@ -4,6 +4,7 @@ import { createRoot } from "react-dom/client";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import ms from "ms";
 
 import App from "./App.tsx";
 import "./index.css";
@@ -15,8 +16,8 @@ const queryClient = new QueryClient({
       refetchOnWindowFocus: false,
       retry: 1,
       retryDelay: (attemptIndex) => Math.min(1000 * 2 ** attemptIndex, 30000), // Exponential backoff
-      staleTime: 1000 * 60 * 5, // 5 minutes
-      cacheTime: 1000 * 60 * 30, // 30 minutes
+      staleTime: ms("5 minutes"), // 5 minutes
+      cacheTime: ms("30 minutes"), // 30 minutes
     },
   },
 });
