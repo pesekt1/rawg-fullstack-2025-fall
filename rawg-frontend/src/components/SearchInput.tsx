0011f5041f4671df27f5da78e7b8f4b98299f6a0
pesekt1 <pesekt@gmail.com>
2025-11-17 20:28:15 +0100

    navigate to home page after search

--- rawg-frontend/src/components/SearchInput.tsx
+++ rawg-frontend/src/components/SearchInput.tsx
@@ -1,20 +1,25 @@
 import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
 
 import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 
 import useGameQueryStore from "../store";
 
 const SearchInput = () => {
-  const onSearch = useGameQueryStore((s) => s.setSearchText);
+  const setSearchText = useGameQueryStore((s) => s.setSearchText);
 
   const ref = useRef<HTMLInputElement>(null);
+
+  const navigate = useNavigate();
 
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
-        onSearch(ref.current?.value || "");
+        setSearchText(ref.current?.value || "");
+        navigate("/");
+        if (ref.current) ref.current.value = "";
       }}
     >
       <InputGroup>
